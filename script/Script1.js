2bc9083f821ac619b1fb1a02f17299ff3953aeea
PedroVazquez523 <patquerp@gmail.com>
2025-06-18 17:00:28 -0600

    Add files via upload

---
+++ script/Script1.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+var hoy = new Date();
+dia = hoy.getDate();
+mes = hoy.getMonth()+1;
+anyo = hoy.getYear() + 1;
+anyo = hoy.getFullYear();
+alert("hoy es " + dia + " del " + mes + " de  " + anyo)
